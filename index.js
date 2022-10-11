e9fadf283e23d9670e596cd5c4a6635f673af451
Local_PC <sv7230@srmist.edu.in>
2022-10-11 14:10:57 +0530

    To Do List Created using JS

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000,1 =>{
+
+
+    console.log("Server started on 3000"),
+});
---
+++ todolist.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+let toDoArr = [
+  {
+    item: 'Make a To-Do app',
+    id: 0,
+  },
+];
+
+app.get('/', function (req, res) {
+  res.status(200).json({ data: toDoArr, message: 'Data fetched!' });
+});
+
+app.post('/', function (req, res) {
+  toDoArr.push(req.body);
+  res.status(200).json({ data: toDoArr });
+});
+
+app.delete('/', function (req, res) {
+  toDoArr = toDoArr.filter(function (el) {
+    return req.body.id !== el.id;
+  });
+  res.status(200).json({ data: toDoArr });
+});
+
+app.put('/', function (req, res) {
+  toDoArr = toDoArr.filter(function (el) {
+    return req.body.id !== el.id;
+  });
+  toDoArr.push(req.body);
+  res.status(200).json({ data: toDoArr });
+})
+
+app.listen(3000, () => {
+  console.log('Server started at PORT: 3000');
+});
